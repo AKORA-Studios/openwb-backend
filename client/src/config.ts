9ba8941c7a2cae1c96d93c0b29281e2b631f341e
Maf <dragoncatm@gmail.com>
2022-01-29 19:26:59 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+
+//Load enviroment variables
+config();
+
+export default {
+    MQTT_URL: process.env.MQTT_URL ?? 'mqtt://test.mosquitto.org',
+    MQTT_USER: process.env.MQTT_USER ?? '',
+    MQTT_PASSWORD: process.env.MQTT_PASSWORD ?? '',
+};
