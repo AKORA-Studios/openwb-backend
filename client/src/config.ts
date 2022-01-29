b5090320a7a5774cd19211b7da0cf2a6f496a4c3
Maf <dragoncatm@gmail.com>
2022-01-29 19:39:49 +0100

    Add basic fastify scaffhold

--- client/src/config.ts
+++ client/src/config.ts
@@ -7,4 +7,5 @@ export default {
     MQTT_URL: process.env.MQTT_URL ?? 'mqtt://test.mosquitto.org',
     MQTT_USER: process.env.MQTT_USER ?? '',
     MQTT_PASSWORD: process.env.MQTT_PASSWORD ?? '',
+    PORT: process.env.PORT ?? 3000,
 };
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,16 @@
 import config from './config';
+import Fastify from 'fastify';
 
-console.log(config);
+const fastify = Fastify({
+    logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+    reply.type('application/json').code(200);
+    return { hello: 'world' };
+});
+
+fastify.listen(config.PORT, (err, address) => {
+    if (err) throw err;
+    // Server is now listening on ${address}
+});
