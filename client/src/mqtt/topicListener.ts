a4e23853121a9327523a8bb879e11edddbb78d99
Maf <65976562+A-K-O-R-A@users.noreply.github.com>
2022-01-30 17:45:49 +0100

    fix typings

---
+++ client/src/mqtt/topicListener.ts
@@ -0,0 +1,55 @@
+import EventEmitter from 'events';
+import { topics } from './topics';
+
+type SubscribeEvents = topics.LESEND;
+type PubishEvents = topics.SCHREIBEND;
+
+export declare interface TopicListener {
+    on<K extends keyof SubscribeEvents>(
+        event: K,
+        listener: (message: SubscribeEvents[K]) => any
+    ): this;
+    on<S extends string | symbol>(
+        event: Exclude<S, keyof SubscribeEvents>,
+        listener: (message: any[]) => any
+    ): this;
+
+    once<K extends keyof SubscribeEvents>(
+        event: K,
+        listener: (message: SubscribeEvents[K]) => any
+    ): this;
+    once<S extends string | symbol>(
+        event: Exclude<S, keyof SubscribeEvents>,
+        listener: (message: any[]) => any
+    ): this;
+
+    emit<K extends keyof SubscribeEvents>(
+        event: K,
+        message: SubscribeEvents[K]
+    ): boolean;
+    emit<S extends string | symbol>(
+        event: Exclude<S, keyof SubscribeEvents>,
+        message: unknown[]
+    ): boolean;
+
+    off<K extends keyof SubscribeEvents>(
+        event: K,
+        listener: (message: SubscribeEvents[K]) => any
+    ): this;
+    off<S extends string | symbol>(
+        event: Exclude<S, keyof SubscribeEvents>,
+        listener: (message: any[]) => any
+    ): this;
+
+    removeAllListeners<K extends keyof SubscribeEvents>(event?: K): this;
+    removeAllListeners<S extends string | symbol>(
+        event?: Exclude<S, keyof SubscribeEvents>
+    ): this;
+
+    publish<K extends keyof PubishEvents>(
+        event: K,
+        message: PubishEvents[K]
+    ): boolean;
+}
+
+export class TopicListener extends EventEmitter {}
