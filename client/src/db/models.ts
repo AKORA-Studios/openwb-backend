74d54a78e6a3e85053f2c6ba1467b5715916eac8
Maf <dragoncatm@gmail.com>
2022-01-30 21:54:46 +0100

    Add typegoose

--- client/src/db/models.ts
+++ client/src/db/models.ts
@@ -1,38 +1,59 @@
 import { prop, pre, post, getModelForClass } from '@typegoose/typegoose';
+import { Units } from '../mqtt/topics';
 
-@pre<KittenClass>('save', function () {
-    this.isKitten = this.age < 1;
-})
-@post<KittenClass>('save', function (kitten) {
-    console.log(
-        kitten.isKitten ? 'We have a kitten here.' : 'We have a big kitty here.'
-    );
-})
-class KittenClass {
-    @prop()
-    public name?: string;
+class Ladepunkt {
+    @prop({ required: true })
+    public ADirectModeAmps!: Units.Ampere; //Sofort laden Soll Stromstärke
 
-    @prop()
-    public species?: string;
+    @prop({ required: true })
+    public PercentDirectChargeModeSoc!: Units.SoC; //Bis zu wieviel % im Sofort Laden Untermodus SoC geladen wird
 
-    @prop()
-    public age?: number;
+    @prop({ required: true })
+    public strChargePointName!: string; //Name des Ladepunktes
 
-    @prop({ default: false })
-    public isKitten?: boolean;
+    @prop({ required: true })
+    public boolChargeAtNight!: Units.bool; //Nachtladen aktiv 1 / 0
+
+    @prop({ required: true })
+    public boolDirectModeChargekWh!: Units.bool; //Gibt an ob der Sofort Laden Untermodus Lademenge aktiv ist
+
+    @prop({ required: true })
+    public APhase1!: Units.Ampere; //Stromstärke in Ampere
+
+    @prop({ required: true })
+    public APhase2!: Units.Ampere; //Stromstärke in Ampere
+
+    @prop({ required: true })
+    public APhase3!: Units.Ampere; //Stromstärke in Ampere
+
+    @prop({ required: true })
+    public VPhase1!: Units.Volts; //Spannung in Volt
+
+    @prop({ required: true })
+    public VPhase2!: Units.Volts; //Spannung in Volt
+
+    @prop({ required: true })
+    public VPhase3!: Units.Volts; //Spannung in Volt
+
+    @prop({ required: true })
+    public kWhCounter!: Units.kWh; //Zählerstand in Wh an Ladepunkt 1
+
+    @prop({ required: true })
+    public W!: Units.Watt; //Ladeleistung in Watt
+
+    @prop({ required: true })
+    public boolPlugStat!: Units.bool; //Steckererkennung = steckend
+
+    @prop({ required: true })
+    public AConfigured!: Units.Ampere; //Ampere mit denen geladen werden soll
+
+    @prop({ required: true })
+    public ChargeMode!: Units.ChargeMode; //Lademodus, 0 = Sofort Laden (Direct), 1 = Min und PV, 2 = Nur PV, 3 = Stop, 4 = Standby
 }
 
-const KittenModel = getModelForClass(KittenClass);
+const LadepunktModel = getModelForClass(Ladepunkt);
 
-const doc = new KittenModel({
-    name: 'SomeCat',
-    species: 'SomeSpecies',
-    age: 0,
+const doc = new LadepunktModel({
+    
 });
-await doc.save(); // this should output "We have a kitten here."
-const doc = new KittenModel({
-    name: 'SomeCat',
-    species: 'SomeSpecies',
-    age: 2,
-});
-await doc.save(); // this should output "We have a big kitty here."
+doc.save(); // this should output "We have a kitten here."
