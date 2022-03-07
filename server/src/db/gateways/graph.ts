086ee8fee356e8fcf18123bffd00753aa05caddc
AKORA <dragoncatm@gmail.com>
2022-03-07 22:24:24 +0100

    Owo whats this

--- server/src/db/gateways/graph.ts
+++ server/src/db/gateways/graph.ts
@@ -1,26 +1,13 @@
-import { getManager } from 'typeorm';
 import getLiveValues from '../../api/getLiveValues';
 import config from '../../config';
 import mqttListener from '../../openWB/client';
-import openWB from '../models/graph';
+import Graph from '../models/graph';
 
 mqttListener.on('openWB/system/lastlivevalues', async (str) => {
     const values = await getLiveValues();
     if (!values) return;
 
-    if (config.NODE_ENV === 'production') {
-        const point = new openWB();
-        for (let key in values) {
-            if (key === 'time') {
-                point['time'] = new Date();
-                continue;
-            }
-            //@ts-ignore
-            point[key] = values[key];
-        }
-
-        await getManager().save(point);
-    }
+    if (config.NODE_ENV !== 'production') return;
 });
 
 mqttListener.on('openWB/graph/lastlivevalues', (str) => {});
