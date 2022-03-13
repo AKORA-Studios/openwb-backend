cb9bb1c9322381c7b272f20ab80a00bf1d1aad24
AKORA <dragoncatm@gmail.com>
2022-03-13 15:18:25 +0100

    Add time

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -83,9 +83,14 @@ export const api: FastifyPluginCallback = function (server, opts, done) {
     server.get('/keys', async (request, reply) => {
         reply.type('application/json').code(200);
 
+        const start = new Date().getTime();
+
         let keys = (await redisClient.keys('*')).sort();
         let obj = {} as any;
         for (let key of keys) obj[key] = await getKey(key);
+
+        const timeDiff = new Date().getTime() - start;
+        obj.time = timeDiff;
 
         return obj;
     });
