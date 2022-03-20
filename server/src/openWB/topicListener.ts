7d29f4f8bb3ff3602c53371bf3d92f439aeb0c46
AKORA <dragoncatm@gmail.com>
2022-03-20 12:45:45 +0100

    change to LF end of line

--- server/src/openWB/topicListener.ts
+++ server/src/openWB/topicListener.ts
@@ -16,10 +16,7 @@ export class TopicListener extends EventEmitter {
         super();
         this.client = client;
     }
-    public publish<K extends keyof PublishEvents>(
-        event: K,
-        message: PublishEvents[K]
-    ): Promise<void> {
+    public publish<K extends keyof PublishEvents>(event: K, message: PublishEvents[K]): Promise<void> {
         //@ts-ignore
         let type = typeof topicMap.SCHREIBEND[event];
 
@@ -33,51 +30,22 @@ export class TopicListener extends EventEmitter {
 }
 
 export declare interface TopicListener {
-    on<K extends keyof SubscribeEvents>(
-        event: K,
-        listener: (message: SubscribeEvents[K]) => any
-    ): this;
-    on<S extends string | symbol>(
-        event: Exclude<S, keyof SubscribeEvents>,
-        listener: (message: any[]) => any
-    ): this;
+    on<K extends keyof SubscribeEvents>(event: K, listener: (message: SubscribeEvents[K]) => any): this;
+    on<S extends string | symbol>(event: Exclude<S, keyof SubscribeEvents>, listener: (message: any[]) => any): this;
 
-    once<K extends keyof SubscribeEvents>(
-        event: K,
-        listener: (message: SubscribeEvents[K]) => any
-    ): this;
-    once<S extends string | symbol>(
-        event: Exclude<S, keyof SubscribeEvents>,
-        listener: (message: any[]) => any
-    ): this;
+    once<K extends keyof SubscribeEvents>(event: K, listener: (message: SubscribeEvents[K]) => any): this;
+    once<S extends string | symbol>(event: Exclude<S, keyof SubscribeEvents>, listener: (message: any[]) => any): this;
 
-    emit<K extends keyof SubscribeEvents>(
-        event: K,
-        message: SubscribeEvents[K]
-    ): boolean;
-    emit<S extends string | symbol>(
-        event: Exclude<S, keyof SubscribeEvents>,
-        message: unknown[]
-    ): boolean;
+    emit<K extends keyof SubscribeEvents>(event: K, message: SubscribeEvents[K]): boolean;
+    emit<S extends string | symbol>(event: Exclude<S, keyof SubscribeEvents>, message: unknown[]): boolean;
 
-    off<K extends keyof SubscribeEvents>(
-        event: K,
-        listener: (message: SubscribeEvents[K]) => any
-    ): this;
-    off<S extends string | symbol>(
-        event: Exclude<S, keyof SubscribeEvents>,
-        listener: (message: any[]) => any
-    ): this;
+    off<K extends keyof SubscribeEvents>(event: K, listener: (message: SubscribeEvents[K]) => any): this;
+    off<S extends string | symbol>(event: Exclude<S, keyof SubscribeEvents>, listener: (message: any[]) => any): this;
 
     removeAllListeners<K extends keyof SubscribeEvents>(event?: K): this;
-    removeAllListeners<S extends string | symbol>(
-        event?: Exclude<S, keyof SubscribeEvents>
-    ): this;
+    removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof SubscribeEvents>): this;
 
-    publish<K extends keyof PublishEvents>(
-        event: K,
-        message: PublishEvents[K]
-    ): Promise<void>;
+    publish<K extends keyof PublishEvents>(event: K, message: PublishEvents[K]): Promise<void>;
 
     destroy(): Promise<void>;
 }
