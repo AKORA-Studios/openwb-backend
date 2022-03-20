700ab5276f96772b2efdc1367a47b10e670a0841
AKORA <dragoncatm@gmail.com>
2022-03-21 00:52:09 +0100

    :smile: Add @me Endpoint

--- server/src/api/endpoints.ts
+++ server/src/api/endpoints.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginCallback } from 'fastify';
+import { UserRequest } from '.';
 import { getKey } from '../db/redis';
 import { getGlobals, getLadelog, getLadepunkt, getLiveValues, getRFID, getVerbrauch } from '../lib';
+import { findUser, generateJWT } from './auth';
 import { keyRoute, lademodusRoute, lademodusSetRoute, restRoute } from './routes';
 
 export const loadEndpoints: FastifyPluginCallback = (server) => {
@@ -37,6 +39,36 @@ export const loadEndpoints: FastifyPluginCallback = (server) => {
         return {
             log: await getLadelog(),
         };
+    });
+
+    //Authentication
+    //
+    //
+    //
+    //
+    server.route({
+        url: '/@me',
+        method: 'GET',
+        preHandler: server.auth([server.verifyJWT]),
+        handler: async (req: UserRequest, repl) => {
+            repl.type('application/json').code(200);
+            return req.params.user;
+        },
+    });
+
+    server.route({
+        url: '/login',
+        method: 'POST',
+        handler: async (req, repl) => {
+            const user = await findUser((req.body as any).username, (req.body as any).password);
+
+            if (!user) {
+                repl.code(403);
+                return { message: 'Invalid Credentials' };
+            }
+
+            return await generateJWT(user);
+        },
     });
 };
 
