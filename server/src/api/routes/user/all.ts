81e0531d098bfa6dd799ee2e98d1b6e30529f189
AKORA <dragoncatm@gmail.com>
2022-03-30 16:30:35 +0200

    :tram: Add /user/all route

---
+++ server/src/api/routes/user/all.ts
@@ -0,0 +1,19 @@
+import { UserRequest } from '../..';
+import User from '../../../db/models/User';
+import { MyServer } from '../../endpoints';
+
+export default (server: MyServer) => {
+    server.route({
+        url: '/all',
+        method: 'GET',
+        preHandler: server.auth([server.verifyJWT, server.verifyAdmin]),
+        handler: async (req: UserRequest, repl) => {
+            repl.type('application/json').code(200);
+            return (await User.findAll()).map((u) => ({
+                username: u.username,
+                tagName: u.tagName,
+                admin: u.admin,
+            }));
+        },
+    });
+};
--- server/src/api/routes/user/index.ts
+++ server/src/api/routes/user/index.ts
@@ -1,10 +1,12 @@
 import { MyServer } from '../../endpoints';
 import meRoute from './@me';
+import allRoute from './all';
 
 export default (server: MyServer) => {
     server.register(
         (server, opts, done) => {
             meRoute(server);
+            allRoute(server);
 
             done();
         },
