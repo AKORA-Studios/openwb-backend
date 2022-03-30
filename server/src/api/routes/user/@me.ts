eea8b82a544251e1b0a745f6bba588b8dd252334
AKORA <dragoncatm@gmail.com>
2022-03-30 16:31:18 +0200

    :tram: Add /user/@me route

--- server/src/api/routes/user/@me.ts
+++ server/src/api/routes/user/@me.ts
@@ -1,7 +1,9 @@
 import { UserRequest } from '../..';
 import { MyServer } from '../../endpoints';
+import S from 'fluent-json-schema';
+import User from '../../../db/models/User';
 
-export const meRoute = (server: MyServer) => {
+export default (server: MyServer) => {
     server.route({
         url: '/@me',
         method: 'GET',
@@ -11,5 +13,29 @@ export const meRoute = (server: MyServer) => {
             return req.params.user;
         },
     });
+
+    const bodyJsonSchema = S.object().prop('password', S.string().required());
+
+    // Note that there is no need to call `.valueOf()`!
+    const schema = {
+        body: bodyJsonSchema,
+    };
+
+    server.route({
+        url: '/@me',
+        method: 'PATCH',
+        schema,
+        preHandler: server.auth([server.verifyJWT]),
+        handler: async (req: UserRequest, repl) => {
+            repl.type('application/json').code(200);
+
+            const user = User.findOne({
+                where: {
+                    username: req.params.user.username,
+                },
+            });
+
+            return req.params.user;
+        },
+    });
 };
-export default meRoute;
