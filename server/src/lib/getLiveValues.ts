4d314cd257f1baf465df312f2ac10f029d9b7b34
AKORA <dragoncatm@gmail.com>
2023-03-14 23:07:47 +0100

    correct value order

--- server/src/lib/getLiveValues.ts
+++ server/src/lib/getLiveValues.ts
@@ -7,36 +7,38 @@ export async function getLiveValues() {
 
     let arr = str.split(',').map((s: any) => (isNaN(Number(s)) ? s : Number(s)));
 
+    // mapping von www\html\openWB\graphing.sh:69
     const mapped = {
         time: ((await getKey('openWB/system/Timestamp')) as number) * 1000,
         evu: arr[1] as number,
-        ladeleistungGesamt: arr[2] as number,
-        PV: arr[3] as number,
-        LadeleistungLP2: arr[4] as number,
-        LadeleistungLP3: arr[5] as number,
-        Speicherleistung: arr[6] as number,
-        SpeicherSoC: arr[7] as number,
-        SoCLp1: arr[8] as number,
-        SoCLp2: arr[9] as number,
-        Hausverbrauch: arr[10] as number,
-        VB1: arr[11] as number,
-        VB2: arr[12] as number,
+        ladeleistung: arr[2] as number,
+        pv: arr[3] as number,
+        LadeleistungLP1: arr[4] as number,
+        LadeleistungLP2: arr[5] as number,
+        ladeleistung2: arr[6] as number,
+        speicherleistung: arr[7] as number,
+        SpeicherSoC: arr[8] as number,
+        SoC: arr[9] as number,
+        SoC1: arr[10] as number,
+        hausverbrauch: arr[11] as number,
+        VB1: arr[12] as number,
+        VB2: arr[13] as number,
     };
 
     return {
         /** UTC */
         time: mapped.time,
         evu: mapped.evu,
-        photovoltaik: mapped.PV,
-        hausverbrauch: mapped.VB1,
-        ladeleistung: mapped.ladeleistungGesamt,
+        photovoltaik: mapped.pv,
+        hausverbrauch: mapped.hausverbrauch,
+        ladeleistung: mapped.ladeleistung,
 
+        ladeleistung_lp1: mapped.LadeleistungLP1,
         ladeleistung_lp2: mapped.LadeleistungLP2,
-        ladeleistung_lp3: mapped.LadeleistungLP3,
-        speicherleistung: mapped.Speicherleistung,
+        speicherleistung: mapped.speicherleistung,
         speicher_soc: mapped.SpeicherSoC,
-        soc_lp1: mapped.SoCLp1,
-        soc_lp2: mapped.SoCLp2,
+        soc_lp1: mapped.SoC,
+        soc_lp2: mapped.SoC1,
         vb1: mapped.VB1,
         vb2: mapped.VB2,
     };
