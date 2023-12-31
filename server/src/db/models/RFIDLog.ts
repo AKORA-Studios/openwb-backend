8b8163b4fb42588ac20637edbee381fb1ade7ad0
AKORA <dragoncatm@gmail.com>
2023-12-31 12:17:19 +0100

    Use correct topic for setting direct charge sub mode and settings

--- server/src/db/models/RFIDLog.ts
+++ server/src/db/models/RFIDLog.ts
@@ -73,24 +73,37 @@ if (config.PROD) {
         // 0 = Sofort Laden (Direct), 1 = Min und PV, 2 = Nur PV, 3 = Stop, 4 = Standby
         // 'openWB/set/ChargeMode': Units.ChargeMode;
         await mqttClient.publish('openWB/set/ChargeMode', user.chargeMode! + '');
+        // copied from https://github.com/snaptec/openWB/blob/25a4ab0b2b861639cdda4ad52b34e7530b3eec51/web/display/gauges/live.js#L1053
+        await mqttClient.publish('openWB/global/ChargeMode', user.chargeMode! + '');
 
         // If direct charge is selected also set the according settings
         if (user.chargeMode === 0) {
+            // These topics were copied from runs/mqttsub.py
+
             // Setzt den Sofort Laden (Direct) Untermodus, Int 0 = Aus, 1 = kWh Laden, 2 = SoC Laden
-            // 'openWB/set/lp1/DirectChargeSubMode': Units.ChargeSubMode;
-            await mqttClient.publish('openWB/set/lp/1/DirectChargeSubMode', user.subMode + '');
+            // 'openWB/config/set/sofort/lp/1/chargeLimitation': Units.ChargeSubMode;
+            await mqttClient.publish(
+                'openWB/config/set/sofort/lp/1/chargeLimitation',
+                user.subMode + ''
+            );
 
             // Setzt den Sofort Laden (Direct) Untermodus SoC Wert bis zu dem geladen werden soll, Int 1 - 100
-            // 'openWB/set/lp1/DirectChargeSoc': Units.SoC;
-            await mqttClient.publish('openWB/set/lp/1/DirectChargeSoc', user.chargeSoc + '');
+            // 'openWB/config/set/sofort/lp/1/socToChargeTo': Units.SoC;
+            await mqttClient.publish(
+                'openWB/config/set/sofort/lp/1/socToChargeTo',
+                user.chargeSoc + ''
+            );
 
             // Ampere mit denen im Sofortladen Modus geladen werden soll, Int 6-32
-            // 'openWB/set/lp1/DirectChargeAmps': Units.Ampere;
-            await mqttClient.publish('openWB/set/lp/1/DirectChargeAmps', user.chargeAmps + '');
+            // 'openWB/config/set/sofort/lp/1/current': Units.Ampere;
+            await mqttClient.publish('openWB/config/set/sofort/lp/1/current', user.chargeAmps + '');
 
             // Setzt die Lademenge in kWh für den Sofort Laden Untermodus Lademenge, Int 1-100
-            //'openWB/set/lp1/kWhDirectChargeToCharge': Units.kWh;
-            await mqttClient.publish('openWB/set/lp1/kWhDirectChargeToCharge', user.chargeKwh + '');
+            //'openWB/config/set/sofort/lp/1/energyToCharge': Units.kWh;
+            await mqttClient.publish(
+                'openWB/config/set/sofort/lp/1/energyToCharge',
+                user.chargeKwh + ''
+            );
         }
     });
 } else {
